6c2edcdb253f8b80cc37807bba325ac7ad3df18a
Jacob McKitrick <jakemckit@gmail.com>
2022-11-20 02:11:01 -0500

    Added exercises.js
    File with exercises after the daily lesson

---
+++ exercises.js
@@ -0,0 +1,8 @@
+//This will be a file containing the exercises from each day for review.
+
+// Days 1 and 2 are not added yet, 
+
+//Day 3 Lvl2
+
+//Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+//By clicking the button on screen "Calc Area Circle"
--- variables.js
+++ variables.js
@@ -1,7 +1,19 @@
 //variables created for 30 Days Of JavaScript 
 //this is starting from day 3
+
+const PI = 3.14
+const now = new Date()
+const year = now.getFullYear() // return year
+const month = now.getMonth() + 1 // return month(0 - 11)
+const date = now.getDate() // return date (1 - 31)
+const hours = now.getHours() // return number (0 - 23)
+const minutes = now.getMinutes() // return number (0 -59)
+
+console.log(`${year}/${month}/${date} ${hours}:${minutes}`) // 4/1/2020 0:56
+
 /*this function will calculate area of a triangle 
 and rectangle by asking the user for the base and height*/
+
 function calcArea() {
     let iBase = prompt(`Enter the Base`, `Here`)
     let iHeight = prompt(`Enter the Height`, `Here`)
@@ -12,4 +24,21 @@ function calcArea() {
     console.log(`The area of a rectangle with chosen dimensions would be ${rArea}`)
     console.log(`The perimeter of a rectangle with chosen dimensions would be ${rPerim}`)
 
+}
+
+function calcAreaCir() {
+    let cRad = prompt(`Enter the Radius of the Circle please`, `Here`)
+    cArea = (c = 2 * +PI * +cRad)
+    console.log(`The Area of the Circle with the chosen dimensions would be ${cArea}`)
+}
+
+function calcSlope() {
+    let x1 = prompt(`Please provide 1st pair 1st digit`)
+    let y1 = prompt(`Please provide 1st pair 2nd digit`)
+    let x2 = prompt(`Please provide 2nd pair 1st digit`)
+    let y2 = prompt(`Please provide 2nd pair 2nd digit`)
+    slopeForm1 = +y2 - +y1
+    slopeForm2 = +x2 - +x1
+    // let slopeForm = parseInt((y2 - y1)`/`(x2 - x1))
+    console.log(`${slopeForm1}\/${slopeForm2}`)
 }
