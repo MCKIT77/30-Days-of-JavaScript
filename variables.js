07e7ff72ddc24a3de1c153b58a89d56f89e23f07
Jacob McKitrick <jakemckit@gmail.com>
2023-01-19 13:53:38 -0500

    one day
    small change

--- variables.js
+++ variables.js
@@ -889,7 +889,13 @@ allParagraphs[3].innerHTML = 'Fourth Paragraph'
 
 allParagraphs[3].setAttribute('class', 'fourth-paragraph')
 allParagraphs[3].id = 'fourthParagraph'
-
+//creating elements
+let fifth_paragraph
+fifth_paragraph = document.createElement('p')
+fifth_paragraph.className = 'fifth-paragraph'
+fifth_paragraph.id = 'fifthParagraph'
+fifth_paragraph.textContent = 'Fifth Paragraph'
+document.body.appendChild(fifth_paragraph)
 
 const getNodeListContent = () => {
     for (i = 0; i < allParagraphs.length; i++) {
@@ -954,3 +960,5 @@ function getRandomColor() {
     }
     return color;
 }
+
+
